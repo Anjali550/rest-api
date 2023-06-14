0d2739f2e19ec3fed31a29d6a4e652563133e04b
Anjali550 <sainianjali1097@gmail.com>
2023-06-14 22:46:58 +0530

    first commit

--- script.js
+++ script.js
@@ -9,34 +9,33 @@ const getUsers = () => {
             
             result.forEach(data => {
                 container.innerHTML +=
+           
+
             `<div id=${data.id} class="data">
-                <h3>Name: ${data.name}</h3>
-                <p>"username": "Bret"</p>
-                <p>"email": "Sincere@april.biz"</p>
-                <div class="details">
-                    <div class="address">
-                        <p>"address":
-                            "street": "Kulas Light",
-                            "suite": "Apt. 556",
-                            "city": "Gwenborough",
-                            "zipcode": "92998-3874",
-                            "geo": {
-                            "lat": "-37.3159",
-                            "lng": "81.1496"
-                            }</p>
-                    </div>
-                    <div class="phone">
-                        <h5>"phone": "1-770-736-8031 x56442",</h5>
-                        <h6>"website": "hildegard.org"</h6>
-                    </div>
-                    <div class="company">
-                        <h4>"company": {
-                            ${data.company.name},
-                            "catchPhrase": "Multi-layered client-server neural-net",
-                            "bs": "harness real-time e-markets"
-                            }</h4>
-                    </div>
-            </div>`
+            <div class="first">
+            <button>${data.id}</button>
+             <h3>${data.name}</h3>
+             
+             <p>${data.email}</p>
+            </div>
+             <div class="details">
+                 
+                     <h3>Profile Details</h3>
+                        <p>User name:-  ${  data.username}</p>
+                        <p>city:-${data.address.city}</p>
+                        
+                        <p> Website:- ${data.website}</p>
+                        <p>contact no:-${data.phone}</p>
+                   
+                        <p>company name:- ${data.company.name}</p>
+
+                         
+                        
+                 </div>
+               </div> 
+                
+         </div>
+            `
             })
         })
 }
